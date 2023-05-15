c463e8c7992997eff47d4f947971a09758633f95
gunerselamoglu <gyuners@hotmail.com>
2023-05-15 21:40:44 +0300

    update 5.1

--- assets/js/mainFull.js
+++ assets/js/mainFull.js
@@ -965,19 +965,19 @@ let anchors = ['giris', 'merhaba', 'dogum-gunu', 'birsey-duydum', 'golde-yuruyus
       // }
 			
 			// Sayfadan ayrıldığında hangi sayfaya gittiğini kontrol ederek ilgili animasyonu çağırıyoruz.
-			debugger;
-			windowWidth > 1023 && eval(`page${destination}()`);
+			// debugger;
+			// windowWidth > 1023 && eval(`page${destination}()`);
 
-			// Section menü görünümünü ayarlıyoruz
-			if(destination == 1 || destination == 13) {
-				sectionsOpen.classList.remove('visible');
-				pressKitButton.classList.remove('hidden');
-				headerPorfile.classList.remove('hidden');
-			} else {
-				sectionsOpen.classList.add('visible');
-				pressKitButton.classList.add('hidden');
-				headerPorfile.classList.add('hidden');
-			}
+			// // Section menü görünümünü ayarlıyoruz
+			// if(destination == 1 || destination == 13) {
+			// 	sectionsOpen.classList.remove('visible');
+			// 	pressKitButton.classList.remove('hidden');
+			// 	headerPorfile.classList.remove('hidden');
+			// } else {
+			// 	sectionsOpen.classList.add('visible');
+			// 	pressKitButton.classList.add('hidden');
+			// 	headerPorfile.classList.add('hidden');
+			// }
 
 			// Sayfadan ayrıldığında ayrıldığı sayfanın animasyonunu resetliyoruz böylece kullanıcı bu sayfaya tekrar geldiğinde animasyon yeniden başlıyor olacak
 			if(windowWidth > 1023) {
@@ -1009,23 +1009,23 @@ let anchors = ['giris', 'merhaba', 'dogum-gunu', 'birsey-duydum', 'golde-yuruyus
 				},1700);
 			}
 			// Section çemberi animasyonu
-			sectionsList.forEach(elem => elem.classList.remove('active'));
-			sectionsList[destination - 1].classList.add('active');
-			pageOrder.innerHTML = destination - 1;
-			sectionsCircle.style.cssText = `stroke-dasharray : ${(destination - 1) * 8.9}px , 108.389px`;
+			// sectionsList.forEach(elem => elem.classList.remove('active'));
+			// sectionsList[destination - 1].classList.add('active');
+			// pageOrder.innerHTML = destination - 1;
+			// sectionsCircle.style.cssText = `stroke-dasharray : ${(destination - 1) * 8.9}px , 108.389px`;
 		},
 		afterLoad: function (anchorLink, index, origin) {
-			activeSection = index;
-			let activetime;
-			sectionList.forEach((elem, indexInner) => {
-				indexInner == index - 1
-        ?  clearTimeout(activetime) + (activetime = setTimeout(() => sectionList[index - 1].classList.add('getFront'), 100))
-				:  elem.classList.remove('getFront', 'hide')
-			});
+			// activeSection = index;
+			// let activetime;
+			// sectionList.forEach((elem, indexInner) => {
+			// 	indexInner == index - 1
+      //   ?  clearTimeout(activetime) + (activetime = setTimeout(() => sectionList[index - 1].classList.add('getFront'), 100))
+			// 	:  elem.classList.remove('getFront', 'hide')
+			// });
 
-			voicePlayer.forEach(voice => {voice.pause() ; voice.currentTime = 0});
+			// voicePlayer.forEach(voice => {voice.pause() ; voice.currentTime = 0});
 
-			windowWidth < 1023 && eval(`page${index}()`);
+			// windowWidth < 1023 && eval(`page${index}()`);
 		}
 	});
 }
@@ -1043,19 +1043,19 @@ if(windowWidth > 1023) {
 			$(".js-mobile-menu-close-icon").hide();
 	});
 } else {
-	$(".js-mobile-menu-open-icon").click(function(){
-		$(".header-profile__box").addClass("header-profile__box--show");
-		$(".js-mobile-menu-open-icon").hide();
-		$(".js-mobile-menu-close-icon").show();
-		$(".header-profile").addClass('active');
-	});
+	// $(".js-mobile-menu-open-icon").click(function(){
+	// 	$(".header-profile__box").addClass("header-profile__box--show");
+	// 	$(".js-mobile-menu-open-icon").hide();
+	// 	$(".js-mobile-menu-close-icon").show();
+	// 	$(".header-profile").addClass('active');
+	// });
 
-	$(".js-mobile-menu-close-icon").click(function(){
-			$(".header-profile__box").removeClass("header-profile__box--show");
-			$(".js-mobile-menu-open-icon").show();
-			$(".js-mobile-menu-close-icon").hide();
-			$(".header-profile").removeClass('active');
-	});
+	// $(".js-mobile-menu-close-icon").click(function(){
+	// 		$(".header-profile__box").removeClass("header-profile__box--show");
+	// 		$(".js-mobile-menu-open-icon").show();
+	// 		$(".js-mobile-menu-close-icon").hide();
+	// 		$(".header-profile").removeClass('active');
+	// });
 }
 
 // let currentWidth = window.innerWidth;
