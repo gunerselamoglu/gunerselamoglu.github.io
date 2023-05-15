b7336cc86a065dabdc2815a8fc05429d207d0ad6
gunerselamoglu <gyuners@hotmail.com>
2023-05-15 18:23:50 +0300

    update 4.3

--- assets/js/mainFull.js
+++ assets/js/mainFull.js
@@ -51,20 +51,20 @@ document.onreadystatechange = () => {
 			soundOpen.classList.add('fadeIn');
 		}, 3000);
 		
-		startSound.addEventListener('click', () => {
-			introSection.classList.remove('hide');
-			sound = true;
-			initialization();
-			loadingScreen.classList.add("fadeOut");
-			setTimeout(() => {
-				loadingScreen.remove();
-				page1();
+		// startSound.addEventListener('click', () => {
+		// 	introSection.classList.remove('hide');
+		// 	sound = true;
+		// 	initialization();
+		// 	loadingScreen.classList.add("fadeOut");
+		// 	setTimeout(() => {
+		// 		loadingScreen.remove();
+		// 		page1();
 
-				musicPlayer.play();
-				soundControl.classList.add('play');
-				location.href = '#giris';
-			}, 1000);
-		});
+		// 		musicPlayer.play();
+		// 		soundControl.classList.add('play');
+		// 		location.href = '#giris';
+		// 	}, 1000);
+		// });
 
 		startMute.addEventListener('click', () => {
 			introSection.classList.remove('hide');
@@ -1061,17 +1061,17 @@ if(windowWidth > 1023) {
 let currentWidth = window.innerWidth;
 let isMobile = currentWidth < 1024;
 
-// window.addEventListener('resize', function() {
-//   var width = window.innerWidth;
+window.addEventListener('resize', function() {
+  var width = window.innerWidth;
 
-//   if (width < 1024 && !isMobile) {
-//     location.reload();
-//     isMobile = true;
-//   } else if (width >= 1024 && isMobile) {
-//     location.reload();
-//     isMobile = false;
-//   }
-// });
+  if (width < 1024 && !isMobile) {
+    location.reload();
+    isMobile = true;
+  } else if (width >= 1024 && isMobile) {
+    location.reload();
+    isMobile = false;
+  }
+});
 
 
 // pressKitButton.addEventListener('click', () => {
